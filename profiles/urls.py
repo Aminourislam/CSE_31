53e68fc28d22517456455d905165f80788603816
Aminourislam <mdamenourislam@gmail.com>
2026-09-23 14:12:12 +0600

    password change and some nev problem solved

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'profiles'
@@ -7,6 +8,23 @@ urlpatterns = [
     path('', views.profile_view, name='view'),
     path('edit/', views.profile_edit, name='edit'),
     path('settings/', views.privacy_settings, name='privacy'),
+
+    # Password change
+    path(
+        'change-password/',
+        auth_views.PasswordChangeView.as_view(
+            template_name='profiles/change_password.html',
+            success_url='/profile/change-password/done/',
+        ),
+        name='change_password',
+    ),
+    path(
+        'change-password/done/',
+        auth_views.PasswordChangeDoneView.as_view(
+            template_name='profiles/change_password_done.html',
+        ),
+        name='change_password_done',
+    ),
 
     # AJAX
     path('ajax/load-districts/', views.load_districts, name='ajax_districts'),
